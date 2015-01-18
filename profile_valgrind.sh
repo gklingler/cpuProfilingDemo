3b4d627c20af8879d228602e9dd073b1be694cb7
Gernot Klingler <gernot.klingler@gmail.com>
2015-01-18 15:46:32 +0100

    also compile with debugging information for perf_valgrind.sh

--- profile_valgrind.sh
+++ profile_valgrind.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # build the program (no special flags are needed)
-g++ -std=c++11 cpuload.cpp -o cpuload
+g++ -std=c++11 -g cpuload.cpp -o cpuload
 
 # run the program with callgrind; generates a file profile.callgrind that can be viewed with kcachegrind
 valgrind --tool=callgrind --demangle=yes --callgrind-out-file=profile.callgrind ./cpuload
